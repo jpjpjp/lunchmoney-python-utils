4100e12bb320649bcc5897ef73b755d395b8056a
jpjpjp <github@shipherd.com>
2024-03-27 20:45:56 -0400

    Update process_duplicates to supercede compare_plaid_with_mint.py

--- process_duplicates.py
+++ process_duplicates.py
@@ -4,7 +4,7 @@
     potential duplicates, displaying them to the user interactively.
 
     If the user confirms that one or more of the transactions is a duplicate,
-    the program deletes the duplicate transactions.  
+    the program deletes the duplicate transactions.
 
     *** NOTE THE API DOES NOT YET SUPPORT TRANSACTION DELETION.
         FOR NOW THE FUNCTION lunchmoney_delete_transaction() TAGS
@@ -21,36 +21,36 @@ import os
 from datetime import datetime
 import pandas as pd
 from lib.transactions import (
-    read_or_fetch_lm_transactions,
     lunchmoney_update_transaction,
     lunchmoney_delete_transaction,
 )
-from config.lunchmoney_config import START_DATE_STR, END_DATE_STR, LOOKBACK_LM_DUP_DAYS
+from config.lunchmoney_config import LOOKBACK_LM_DUP_DAYS, CACHE_DIR
+import config.lunchmoney_config as LMC
 
 
-def main():
-    # Fetch the transactions from LunchMoney
-    df = read_or_fetch_lm_transactions(
-        datetime.strptime(START_DATE_STR, "%m/%d/%Y"),
-        datetime.strptime(END_DATE_STR, "%m/%d/%Y"),
-        remove_pending=True,
-        remove_split_parents=True,
-    )
+def find_lunchmoney_duplicates(df, lookback_days=0):
+    """ Identifies potential duplicate transactions
 
-    dup_ids = find_duplicates_for_all_accounts(df, lookback_days=LOOKBACK_LM_DUP_DAYS)
+        If any are found it writes them to an output file for examination
+        and returns True
+    """
+    dup_ids = _find_duplicates_for_all_accounts(df, lookback_days)
 
     if len(dup_ids):
         # We found duplicate, write them to a CSV file to be examined
         dup_df = df[df["id"].isin(dup_ids)]
         today_date_str = datetime.now().strftime("%Y-%m-%d")
         output_file_path = os.path.join(
-            "output", f"marked_as_duplicate_{today_date_str}.csv"
+            CACHE_DIR, f"marked_as_duplicate_{today_date_str}.csv"
         )
         dup_df.to_csv(output_file_path, index=False)
         print(f"Found {len(dup_df)} duplicates. Details written to: {output_file_path}")
+        return True
+
+    return False
 
 
-def find_duplicates_for_all_accounts(
+def _find_duplicates_for_all_accounts(
     df,
     lookback_days=0,
 ):
@@ -67,14 +67,14 @@ def find_duplicates_for_all_accounts(
         df_to_search = df[df["account_display_name"] == account_name].sort_values(
             by="date", ascending=False
         )
-        ids_to_delete = find_duplicates_for_one_account(
+        ids_to_delete = _find_duplicates_for_one_account(
             df_to_search, lookback_days, ids_to_delete
         )
 
     return ids_to_delete
 
 
-def find_duplicates_for_one_account(df, lookback_days, ids_to_delete):
+def _find_duplicates_for_one_account(df, lookback_days, ids_to_delete):
     """Look for duplicates in a dataframe of transactions
     If found ask user to disambiguate, tagging non-duplicates, and
     deleteing duplicates
@@ -130,6 +130,7 @@ def find_duplicates_for_one_account(df, lookback_days, ids_to_delete):
                             "payee",
                             "amount",
                             "account_display_name",
+                            "source",
                             "notes",
                             "tags",
                         ]
@@ -161,13 +162,13 @@ def find_duplicates_for_one_account(df, lookback_days, ids_to_delete):
                         need_user_input = False
                     except ValueError:
                         # Add "Not-Duplicate" tag to transactions
-                        (df, df_copy) = interactive_tag_non_dup(matches, df, df_copy)
+                        (df, df_copy) = _interactive_tag_non_dup(matches, df, df_copy)
                         matches = pd.DataFrame()
                         need_user_input = False
     return ids_to_delete
 
 
-def interactive_tag_non_dup(matches, loc_df1, loc_df2):
+def _interactive_tag_non_dup(matches, loc_df1, loc_df2):
     """Tags each transaction as the dataframe with "Not-Duplicate
     Interactively offers opportunity to update Payee or Notes field
     Updates transaction date in Lunch Money via API
@@ -175,15 +176,20 @@ def interactive_tag_non_dup(matches, loc_df1, loc_df2):
     """
     for index, match in matches.iterrows():
         update_obj = {}
-        resp = input(
-            f"Tagging {index} as non duplicate.  Update Payee or Notes? (p/n/enter for no): "
-        )
-        if resp.lower() == "p":
-            resp2 = input("Enter new Payee text: ")
-            update_obj["payee"] = resp2
-        elif resp.lower() == "n":
-            resp2 = input("Enter new Notes text: ")
-            update_obj["notes"] = resp2
+        if hasattr(LMC, "ASK_UPDATE_NON_DUPS") and LMC.ASK_UPDATE_NON_DUPS:
+            resp = input(
+                f"Tagging {index} with 'Non-Duplicate' tag. "
+                "Update Payee or Notes? (p/n/enter for no): "
+            )
+            if resp.lower() == "p":
+                resp2 = input("Enter new Payee text: ")
+                update_obj["payee"] = resp2
+            elif resp.lower() == "n":
+                resp2 = input("Enter new Notes text: ")
+                update_obj["notes"] = resp2
+        else:
+            print(f"Tagging {index} with 'Non-Duplicate' tag.")
+                
         if "Not-Duplicate" not in match["tags"]:
             if match["tags"]:
                 updated_tags = match["tags"] + ",Not-Duplicate"
@@ -202,6 +208,7 @@ def interactive_tag_non_dup(matches, loc_df1, loc_df2):
                 update_obj,
             )
 
+
         # Update local data stores so we don't do these again
         if index in loc_df1.index:
             loc_df1.loc[index, "tags"].append({"name": "SkipDupCheck"})
@@ -212,4 +219,23 @@ def interactive_tag_non_dup(matches, loc_df1, loc_df2):
 
 
 if __name__ == "__main__":
-    main()
+    from lib.transactions import (
+        read_or_fetch_lm_transactions,
+    )
+    from config.lunchmoney_config import START_DATE_STR, END_DATE_STR
+
+    # Fetch the transactions from LunchMoney
+    df = read_or_fetch_lm_transactions(
+        datetime.strptime(START_DATE_STR, "%m/%d/%Y"),
+        datetime.strptime(END_DATE_STR, "%m/%d/%Y"),
+        remove_pending=True,
+        remove_split_parents=True,
+    )
+
+    dups_found = find_lunchmoney_duplicates(df, lookback_days=LOOKBACK_LM_DUP_DAYS)
+
+    if not dups_found:
+        print('No duplicates found!')
+    else:
+        print("Remove duplicate transactions in LunchMoney by filtering on the "
+              "'Duplicate' tag.")
